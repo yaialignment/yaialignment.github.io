f75becb5b82df176dc2c318ff222075669e7d60b
Jaso1024 <107654508+Jaso1024@users.noreply.github.com>
2025-01-18 10:10:07 -0500

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './pages/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
